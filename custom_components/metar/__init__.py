2bf55115f6ee105a21adf26d021581f6ff1fd6d4
gopher_dave <79770840+daviswieck@users.noreply.github.com>
2024-10-20 20:31:15 -0500

    Update __init__.py

--- custom_components/metar/__init__.py
+++ custom_components/metar/__init__.py
@@ -1,1 +1,17 @@
-"""The metar component."""
+import logging
+
+_LOGGER = logging.getLogger(__name__)
+
+DOMAIN = "metar"
+
+async def async_setup_entry(hass, entry):
+    """Set up the METAR integration from a config entry."""
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    )
+    return True
+
+async def async_unload_entry(hass, entry):
+    """Unload a config entry."""
+    await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+    return True
